1804e3d81a9cbfa31b261d5ca887d9a6d308c039
Adam Mokan <amokan@gmail.com>
2016-09-09 07:40:30 -0700

    removed ununsed espec helper code

--- spec/spec_helper.exs
+++ spec/spec_helper.exs
@@ -5,13 +5,3 @@ ESpec.start
 Enum.each files, fn(file) ->
   Code.require_file "support/#{file}", __DIR__
 end
-
-ESpec.configure fn(config) ->
-	config.before fn ->
-		# {:ok, hello: :world}
-	end
-
-	config.finally fn(shared) ->
-
-	end
-end
